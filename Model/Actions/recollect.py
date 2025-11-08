ea63a01b77aa8c14dc2cd8a80683bc10262e1ee1
Wu Wang Yang <56248896+markymarkwu@users.noreply.github.com>
2025-11-08 09:49:18 +0100

    cmt

--- Model/Actions/recollect.py
+++ Model/Actions/recollect.py
@@ -3,7 +3,7 @@ import faiss
 import faiss.contrib.torch_utils
 
 
-class MemorySearcherFaiss:
+class RecollectFaiss:
     """A FAISS-based searcher for efficient nearest neighbor search."""
     def __init__(self, embed_dim: int, device='gpu') -> None:
         self.device = device
@@ -14,11 +14,12 @@ class MemorySearcherFaiss:
             res = faiss.StandardGpuResources() 
             # Move index to GPU 0
             self.index = faiss.index_cpu_to_gpu(res, 0, self.index)
+        self.temp_memory = None
 
     def update_index(self, memory: torch.Tensor) -> None:
         self.index.reset()
         self.index.add(memory.to(self.device))
 
-    def search(self, query: torch.Tensor, k: int) -> torch.Tensor:
+    def recollect(self, query: torch.Tensor, k: int) -> torch.Tensor:
         D, I = self.index.search(query, k)
         return torch.from_numpy(D).to(self.device), torch.from_numpy(I).to(self.device)
